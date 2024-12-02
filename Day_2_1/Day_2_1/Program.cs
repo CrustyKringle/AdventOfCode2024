7a2d56d39271bb5ce4da13e3186f709b20a4c80e
CrustyKringle <124091355+CrustyKringle@users.noreply.github.com>
2024-12-02 22:58:14 +0100

    Add files via upload

---
+++ Day_2_1/Day_2_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\Users\jenst\Desktop\JENS_AO_24_25\AdventOfCode\Day_2_1\Input.txt";
+
+        var lines = File.ReadAllLines(filePath);
+
+        int safe = 0;
+
+        foreach (var line in lines)
+        {
+            var numbers = line.Split(' ').Select(int.Parse).ToArray();
+
+            safe += IsSafe(numbers) ? 1 : 0;
+            
+        }
+
+        Console.WriteLine($"Safe reports " + safe);
+
+        static bool IsSafe(int[] numbers)
+        {
+            bool isIncreasing = true;
+            bool isDecreasing = true;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int diff = numbers[i + 1] - numbers[i];
+
+                if(Math.Abs(diff) > 3 || diff == 0)
+                    return false;
+
+                if (diff < 0)
+                    isIncreasing = false;
+
+                if (diff > 0)
+                    isDecreasing = false;
+            }
+
+            return isIncreasing || isDecreasing;
+        }
+    }
+}
---
+++ Day_2_2/Day_2_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\Users\jenst\Desktop\JENS_AO_24_25\AdventOfCode\Day_2_1\Input.txt";
+
+        var lines = File.ReadAllLines(filePath);
+
+        int safe = 0;
+
+        foreach (var line in lines)
+        {
+            var numbers = line.Split(' ').Select(int.Parse).ToArray();
+
+            safe += OneException(numbers) ? 1 : 0;
+
+        }
+
+        Console.WriteLine($"Safe reports " + safe);
+
+        static bool IsSafe(int[] numbers)
+        {
+            bool isIncreasing = true;
+            bool isDecreasing = true;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int diff = numbers[i + 1] - numbers[i];
+
+                if (Math.Abs(diff) > 3 || diff == 0)
+                    return false;
+
+                if (diff < 0)
+                    isIncreasing = false;
+
+                if (diff > 0)
+                    isDecreasing = false;
+            }
+
+            return isIncreasing || isDecreasing;
+        }
+
+        static bool OneException(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var filterd = numbers.Where((_, x) => x != i).ToArray(); //Hou alle elementen waarvan de index niet gelijk is aan i.
+                                                                         //"_" De waarde van het element is niet van toepassing enkel de index.
+                if (IsSafe(filterd))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
