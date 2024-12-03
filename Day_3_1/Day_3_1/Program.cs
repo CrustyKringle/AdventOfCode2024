19f96ba60429491d89aa34bf4c1624ff41f2b2d8
CrustyKringle <124091355+CrustyKringle@users.noreply.github.com>
2024-12-03 23:12:05 +0100

    Add files via upload

---
+++ Day_3_1/Day_3_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\Users\jenst\Desktop\JENS_AO_24_25\AdventOfCode\Day_3_1\Input.txt";
+
+        var lines = File.ReadAllLines(filePath);
+
+        string pattern = @"mul\((\d+),(\d+)\)";
+        
+        int result = 0;
+
+        foreach (var line in lines )
+        {
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(line);
+
+            foreach (Match match in matches)
+            {
+                string firstNumber = match.Groups[1].Value;
+                string secondNumber = match.Groups[2].Value;
+
+                if (int.TryParse(firstNumber, out int num1) && int.TryParse(secondNumber, out int num2))
+                {
+                   int product = num1 * num2;
+                   result += product;
+                }
+            }             
+        }
+
+        Console.WriteLine($"Result of the multiplications = {result}");
+    }
+}
